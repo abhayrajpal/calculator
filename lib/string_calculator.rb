fa6e684335c32f96d9475bcb418362d8dd71840d
Abhay Rajpal <abhayrajpal77@gmail.com>
2024-06-08 18:46:12 +0530

    Handle error for negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -9,6 +9,13 @@ class StringCalculator
       delimiter = ",|\n"
     end
 
-    numbers.split(/#{delimiter}/).map(&:to_i).sum
+    number_list = numbers.split(/#{delimiter}/).map(&:to_i)
+    negatives = number_list.select { |n| n < 0 }
+
+    unless negatives.empty?
+      raise "negative numbers not allowed: #{negatives.join(', ')}"
+    end
+
+    number_list.sum
   end
 end
